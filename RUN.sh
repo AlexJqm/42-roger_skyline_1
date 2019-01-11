1c09ca421b259352176598cdf199bfea12b943f9
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2019-01-11 08:54:05 +0100

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,23 +1,26 @@
 #! /bin/bash
 
-echo "[ \033[32mOK\033[0m ]\tAutomatized start."
+echo "[ \033[32mOK\033[0m ]\tRUN.sh start."
 sleep 2
 
 ###################### Add User ######################
 read -p "Choose username for new user: " username
 sudo adduser $username
 sudo usermod -aG sudo $username
+echo "[ \033[32mOK\033[0m ]\tNew user add to sudo group."
+sleep 3
 
 ###################### Start update and install package ######################
 sudo apt-get update
 sudo apt-get upgrade
-
 sudo apt-get install net-tools
 sudo apt-get install iptables-persistent
 sudo apt-get install nano
 sudo apt-get install postfix
 sudo apt-get install mailutils
 sudo apt-get install fail2ban
+echo "[ \033[32mOK\033[0m ]\tOS and packages udapte."
+sleep 3
 
 ###################### Network ######################
 NAMENET=$(netstat -i | grep "enp*" | cut -f1 -d " ")
@@ -26,11 +29,15 @@ sudo mv interfaces /etc/network/
 sudo service networking restart
 sudo ifdown $NAMENET
 sudo ifup $NAMENET
+echo "[ \033[32mOK\033[0m ]\tNetworking configuration and static IP."
+sleep 3
 
 ###################### SSH ######################
 sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
 sudo mv sshd_config /etc/ssh/
 sudo service ssh restart
+echo "[ \033[32mOK\033[0m ]\tSSH configuration."
+sleep 3
 
 ###################### Install scripts ######################
 sudo mkdir /var/scripts/
@@ -38,6 +45,8 @@ sudo mv alert.sh /var/scripts/
 sudo mv update.sh /var/scripts/
 sudo mv /etc/crontab /etc/crontab.old
 sudo mv crontab /etc/
+echo "[ \033[32mOK\033[0m ]\tScripts udapte and alert installed."
+sleep 3
 
 ###################### Mail ######################
 sudo mv /etc/aliases /etc/aliases.old
@@ -47,31 +56,39 @@ sudo mv /etc/postfix/main.cf /etc/postfix/main.cf.old
 sudo mv main.cf /etc/postfix/
 sudo mv virtual /etc/postfix/
 sudo service postfix restart
+echo "[ \033[32mOK\033[0m ]\tMail service configured."
+sleep 3
 
 ###################### Iptables ######################
 #sudo mv rules.v4 /etc/iptables/
 #sudo mv rules.v6 /etc/iptables/
 #sudo service netfilter-persistent restart
+echo "[ \033[32mOK\033[0m ]\tIptables rules added."
+sleep 3
 
 ###################### Fail2Ban ######################
 sudo mv /etc/fail2ban/jail.local /etc/fail2ban/jail.local.old
 sudo mv jail.local /etc/fail2ban/
 sudo service fail2ban restart
+echo "[ \033[32mOK\033[0m ]\tFail2Ban configured."
+sleep 3
 
 ###################### Website ######################
 sudo mv /var/www/html /var/www/html.old
 sudo mv html /var/www/
+echo "[ \033[32mOK\033[0m ]\tWebsite upload."
+sleep 3
 
 ###################### Finish ######################
-
-echo "[ \033[32mOK\033[0m ]\tNew user add to sudo group."
-echo "[ \033[32mOK\033[0m ]\tOS and packages udapte."
-echo "[ \033[32mOK\033[0m ]\tNetworking configuration and static IP."
-echo "[ \033[32mOK\033[0m ]\tSSH configuration."
-echo "[ \033[32mOK\033[0m ]\tScripts udapte and alert installed."
-echo "[ \033[32mOK\033[0m ]\tMail service configured."
-echo "[ \033[32mOK\033[0m ]\tIptables rules added."
-echo "[ \033[32mOK\033[0m ]\tFail2Ban configured."
-echo "[ \033[32mOK\033[0m ]\tWebsite upload."
-
-sleep 2
+echo "Installation finished."
+echo "Reboot in 5 secondes"
+sleep 1
+echo "Reboot in 4 secondes"
+sleep 1
+echo "Reboot in 3 secondes"
+sleep 1
+echo "Reboot in 2 secondes"
+sleep 1
+echo "Reboot in 1 secondes"
+sleep 1
+sudo shutdown -r now
