44f49ff2a9a38b972ace2bfac788bd7d7922fe21
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2019-01-11 07:16:30 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,24 +11,27 @@ sudo usermod -aG sudo $username
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get build-dep build-essential
 sudo apt-get install net-tools
 sudo apt-get install iptables-persistent
-sudo apt-get install sudo
-sudo apt-get install zsh
-sudo apt-get install git
 sudo apt-get install nano
 sudo apt-get install postfix
 sudo apt-get install mailutils
+sudo apt-get install fail2ban
 
 ###################### Network ######################
 mv /etc/network/interfaces /etc/network/interfaces.old
-mv interfaces /etc/network/interfaces
+mv interfaces /etc/network/
 sudo service networking restart
 ifdown $NAMENET
 ifup $NAMENET
 
 ###################### SSH ######################
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
-mv sshd_config /etc/ssh/sshd_config
+mv sshd_config /etc/ssh/
 sudo service ssh restart
+
+###################### Install scripts ######################
+mkdir /var/scripts
+mv alert.sh /var/scripts && mv udpate.sh /var/scritps
+mv /etc/crontab /etc/crontab.old
+mv crontab /etc/
