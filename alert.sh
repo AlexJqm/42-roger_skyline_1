421a291e8af62235943356832180185c68cf8a94
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2019-01-11 07:48:49 +0100

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -3,7 +3,7 @@
 cat /etc/crontab > /var/scripts/cpy
 DIFF=$(diff cpy tmp)
 if [ "$DIFF" != "" ]; then
-        echo "Une modification du fichier crontab a ete effectuer." | mail -s "Alerte modification!" root
+        echo "Une modification du fichier crontab a ete effectuer." | mail -s "Alerte modification!" root@localhost
         rm -rf /var/scripts/tmp
         cp /var/scripts/cpy /var/scripts/tmp
 fi
